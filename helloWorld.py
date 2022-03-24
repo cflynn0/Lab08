d4ff52abaea156eae6ebb979f512fa833532a431
C Flynn <flynncjo@gmail.com>
2022-03-24 19:55:20 -0400

    added username and pass

--- helloWorld.py
+++ helloWorld.py
@@ -1,3 +1,8 @@
-userName = input ("Hello! Please enter your name!")
+userName = input("Hello! Please enter your facebook username! ")
 
-print("Hello", userName, "nice to meet you")
+userPassword = input("Hello! Please enter your facebook password! ")
+
+if(userName.lower() == "conner" and userPassword == "pa55word"):
+    print("Welcome to facebook!")
+else:
+    print("Wrong credentials, try again!")
